9264c3cfaa579620620ef2c5a1fff46bb2faf695
Deryn-CHOI <choi3mao@163.com>
2026-05-19 14:20:32 +0800

    Create test_paywall.py

---
+++ automation/tests/test_paywall.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import sync_playwright
+
+
+def test_countdown_not_reset():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        page.goto("https://betterme-pilates.com")
+
+        # TODO: 跳转到paywall
+
+        countdown_before = page.locator(".countdown").inner_text()
+
+        page.reload()
+
+        countdown_after = page.locator(".countdown").inner_text()
+
+        assert countdown_before != countdown_after
+
+        browser.close()
