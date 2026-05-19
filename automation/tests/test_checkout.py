a7168598d9dcb063057394c772e5e8a0f9c1eea2
Deryn-CHOI <choi3mao@163.com>
2026-05-19 14:21:11 +0800

    Create test_checkout.py

---
+++ automation/tests/test_checkout.py
@@ -0,0 +1,19 @@
+from playwright.sync_api import sync_playwright
+
+
+def test_stripe_blocked():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+
+        page = context.new_page()
+
+        page.route("**/*stripe*", lambda route: route.abort())
+
+        page.goto("https://betterme-pilates.com")
+
+        # TODO: 跳转checkout
+
+        assert "Payment failed" in page.content()
+
+        browser.close()
