4244b775413a3b341bedfba45c178db9dab68122
Deryn-CHOI <choi3mao@163.com>
2026-05-20 16:16:59 +0800

    Create conftest.py

---
+++ automation/tests/conftest.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+import pytest
+
+
+@pytest.fixture
+def page():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context()
+        page = context.new_page()
+        yield page
+        browser.close()
